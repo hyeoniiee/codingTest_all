044ef3148f709734d77dda46ef7557a1a70943cb
hyeoniiee <starjea0126@gmail.com>
2024-11-04 12:59:31 +0900

    feat(PGS):1주차

---
+++ 항해99_4기/1주차/1일차/문자열 내 p와 y의 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    boolean solution(String s) {
+        s = s.toLowerCase();
+
+        int pCount = 0;
+        int yCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == 'p') {
+                pCount++;
+            } else if (ch == 'y') {
+                yCount++;
+            }
+        }
+        return pCount == yCount;
+    }
+}
