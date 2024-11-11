bb3e578311197ec8f9fee48a32b6303f3889937b
heeeiiii <starjea0126@gmail.com>
2024-11-11 22:32:03 +0900

    [Bronze I] Title: 평균, Time: 228 ms, Memory: 18956 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] scores = new int[N];
+
+        int maxScore = 0;
+        for (int i = 0; i < N; i++) {
+            scores[i] = sc.nextInt();
+            if (scores[i] > maxScore) {
+                maxScore = scores[i];
+            }
+        }
+
+        double sum = 0;
+        for (int score : scores) {
+            sum += (double) score / maxScore * 100;
+        }
+
+        double avg = sum / N;
+
+        System.out.println(avg);
+
+    }
+}
