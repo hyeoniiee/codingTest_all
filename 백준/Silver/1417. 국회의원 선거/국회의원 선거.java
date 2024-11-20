fef2b86f86f382bbb89f06c874856e0ca11c25a7
heeeiiii <starjea0126@gmail.com>
2024-11-20 18:59:05 +0900

    [Silver V] Title: 국회의원 선거, Time: 176 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -0,0 +1,30 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int dasomVotes = sc.nextInt();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 1; i < n; i++) {
+            pq.add(sc.nextInt());
+        }
+
+        int bribes = 0;
+
+        while (!pq.isEmpty() && pq.peek() >= dasomVotes) {
+            int maxVotes = pq.poll();
+            maxVotes--;
+            dasomVotes++;
+            bribes++;
+            pq.add(maxVotes);
+        }
+
+        System.out.println(bribes);
+    }
+}
