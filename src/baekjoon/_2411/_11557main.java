2352a26dd4615f9301026323522d96587d6fd173
hyeoniiee <starjea0126@gmail.com>
2024-11-23 12:11:37 +0900

    풀이

---
+++ src/baekjoon/_2411/_11557main.java
@@ -0,0 +1,32 @@
+package baekjoon._2411;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _11557main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            String maxSchool = "";
+            int maxConsumption = 0;
+
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String school = st.nextToken();
+                int consumption = Integer.parseInt(st.nextToken());
+
+                if (consumption > maxConsumption) {
+                    maxConsumption = consumption;
+                    maxSchool = school;
+                }
+            }
+
+            System.out.println(maxSchool);
+        }
+    }
+}
