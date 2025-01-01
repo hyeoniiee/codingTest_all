f513663067c41db710ccac2a18948350c7ef9840
hyeoniiee <starjea0126@gmail.com>
2025-01-02 01:14:50 +0900

    code test

---
+++ src/baekjoon/_2501/_10807main.java
@@ -0,0 +1,24 @@
+package baekjoon._2501;
+
+import java.util.Scanner;
+
+public class _10807main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        int[] numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        int v = scanner.nextInt();
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            if (numbers[i] == v) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
